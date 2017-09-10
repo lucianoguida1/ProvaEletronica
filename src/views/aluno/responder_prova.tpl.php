44892719c6a211cfc01d55ade03da4b82f6e3c63
Setinais13 <vynny.cg@gmail.com>
2017-09-10 13:45:59 -0300

    realizar provas 100%

--- src/views/aluno/responder_prova.tpl.php
+++ src/views/aluno/responder_prova.tpl.php
@@ -7,7 +7,11 @@
 <script src="assets/js/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-<div id="sessao"></div>
+<div style="position: fixed;
+  left: 0;
+  top: 20%;
+  margin-top: -2.5em;
+" id="sessao"></div>
 <script type="text/javascript">
 	
 	$(document).ready(function($) {
@@ -53,8 +57,7 @@
  	        
  	        var horaImprimivel = hr+':' + min + ':' + seg;
  	        
- 	        $("#sessao").html(horaImprimivel);
- 	        $("#tempo").html(tempo);
+ 	        $("#sessao").html("<span class='badge badge-warning pull-right'>Tempo "+horaImprimivel+"</span>");
  	        
  	        setTimeout('startCountdown()',1000);
  	        
