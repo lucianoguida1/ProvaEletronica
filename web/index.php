23e2ea921ba61cc3202849273e8b355a73dcf776
wylliansales <wylliansalles@gmail.com>
2017-10-08 00:34:02 -0300

    Ultima alteração

--- web/index.php
+++ web/index.php
@@ -1,10 +1,7 @@
 <?php
-
 session_start();
 date_default_timezone_set('America/Sao_Paulo');
 require_once '../config/autoload.php';
 require_once '../config/config.php';
-Controller::inicializar(); 
-
-
+Controller::inicializar();
 
